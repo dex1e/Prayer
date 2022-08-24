a5398f43c675335e1685e2ae03f27c638400db26
dex1e <dex1e5@yandex.ru>
2022-08-24 16:50:21 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Hello world!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

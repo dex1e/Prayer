67bf23fa3d2424db3df96e01330ec7e2cdb0bcf6
dex1e <dex1e5@yandex.ru>
2022-08-25 18:52:34 +0600

    feat: constant apiUrl, BASE_URL

---
+++ src/constant/apiUrl.ts
@@ -0,0 +1,4 @@
+export const API_URL = {
+  POST_SIGN_UP: 'auth/sign-up',
+  POST_SIGN_IN: 'auth/sign-in',
+};
---
+++ src/constant/index.ts
@@ -0,0 +1,3 @@
+export {API_URL} from './apiUrl';
+
+export const BASE_URL = 'https://prayer.herokuapp.com/';
